27e1dfcbff5b1a5d84783aa2d0443a67ab31d038
Gianni <BussolettiGianni@gmail.com>
2026-03-19 16:41:16 +0100

    working board

--- script.js
+++ script.js
@@ -8,3 +8,19 @@ const spawnCell = () => {
   }
 }
 spawnCell()
+
+const randomNumber = () => Math.ceil(Math.random() * 90)
+const appendingNumber = document.getElementById("drawn-number")
+
+const estrazioneNum = (event) => {
+  appendingNumber.innerHTML = ""
+  const spanNumber = document.createElement("span")
+  const drawnCell = document.getElementsByClassName("numeri")
+  const stringNum = String(randomNumber())
+  spanNumber.innerText = stringNum
+  appendingNumber.appendChild(spanNumber)
+  for (let i = 0; i < drawnCell.length; i++)
+    if (stringNum === drawnCell[i].innerText) {
+      drawnCell[i].classList.add("drawn-number")
+    }
+}
